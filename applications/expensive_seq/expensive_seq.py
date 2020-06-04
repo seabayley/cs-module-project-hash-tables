6a51b8e18bb6c2486c519ff853d402142061a510
Christopher Bailey <seabayley@outlook.com>
2020-06-04 09:57:45 -0500

    Refactored expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,16 +1,14 @@
 # Your code here
-lookup_table = {}
+c = {}
 
 
 def expensive_seq(x, y, z):
-    global lookup_table
     if x <= 0:
         return y + z
-    if x > 0:
-        if (x, y, z) not in lookup_table:
-            lookup_table[(x, y, z)] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y +
-                                                                                 2, z*2) + expensive_seq(x-3, y+3, z*3)
-    return lookup_table[(x, y, z)]
+    if x > 0 and (x, y, z) not in c:
+        c[(x, y, z)] = expensive_seq(x-1, y+1, z) + \
+            expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+    return c[(x, y, z)]
 
 
 if __name__ == "__main__":
